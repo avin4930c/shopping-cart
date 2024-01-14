ed5f6555f3f2aec09a6a98864a8e3bb931ff09a8
Avinash <123456789+avinash4930c@gmail.com>
2024-01-14 13:26:04 +0530

    completed productDetailPage design

--- src/components/productDetailPage.tsx
+++ src/components/productDetailPage.tsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Collapse from 'react-bootstrap/Collapse';
 import Carousel from 'react-bootstrap/Carousel';
 import './productDetailPage.css';
 
 function ProductDetailPage() {
+    const [open, setOpen] = useState(false);
     return (
         <>
             <section className="product-details px-5">
-                <div className="navBar-product bg-dark text-white d-flex justify-content-between align-items-center px-3">
-                    <div className="navBar-left" style={{fontSize: "24px"}}><i className="bi bi-arrow"></i>Store</div>
-                    <div className="navBar-right" style={{fontSize: "48px"}}>Elden Ring</div>
+                <div className="navBar-product text-white d-flex justify-content-between align-items-center px-3">
+                    <div className="navBar-left" style={{ fontSize: "24px" }}><i className="bi bi-arrow"></i>Store</div>
+                    <div className="navBar-right" style={{ fontSize: "48px" }}>Elden Ring</div>
                 </div>
                 <section className="product-details-main">
                     <div className="product-details-carousel">
@@ -24,17 +28,38 @@ function ProductDetailPage() {
                         </Carousel>
                     </div>
                     <div className="product-details-right">
-                        <div className="product-info-main px-4 text-white">
-                            <div className="product-info-title py-2">About</div>
-                            <div className="product-info-details py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ipsam numquam necessitatibus dolorem possimus sunt beatae voluptatibus saepe est officiis.</div>
+                        <div className="product-info-container">
+                            <div className="product-info-main px-4">
+                                <div className="product-info-title py-2 h3">About</div>
+                                <div className="product-info-details py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione quasi possimus, consequuntur accusantium sint dignissimos corporis explicabo a praesentium voluptate quod eaque? Ad molestiae ullam maiores nobis obcaecati. Quibusdam, qui. Fuga sunt reiciendis nobis blanditiis ex in eligendi deserunt, architecto sit non mollitia sed, unde quam nesciunt esse eos ad, doloremque assumenda! Dolorem iusto illo unde blanditiis quas sit error enim repellat odit vero perferendis, nostrum nihil similique velit incidunt, sint repellendus numquam fuga aliquid hic. Voluptates quod laborum quos hic ad cupiditate aspernatur alias, aliquid iusto suscipit quia quisquam iure laudantium! Eius voluptates quod debitis ipsam impedit tempora deleniti. Odio doloribus magni recusandae accusamus quibusdam consequatur porro repellendus. Voluptate nulla maxime perspiciatis, vero praesentium sapiente vitae voluptas in, aliquid facilis minima explicabo eius quaerat laboriosam expedita porro sunt, exercitationem quo autem iusto quas consectetur placeat dignissimos? Eius ipsa iste, optio dolore accusantium consequatur quaerat minus corporis voluptates amet quam, cumque debitis pariatur ratione quo nobis atque facere perferendis sit. Consequuntur maiores tenetur vel ipsum laboriosam alias molestias saepe numquam ratione aspernatur? Repudiandae reiciendis tempore mollitia cupiditate alias, ratione ea vero perspiciatis dolorem nesciunt! Minima aliquid architecto unde voluptate molestiae totam dolore molestias illo deleniti consequuntur dolorum, aspernatur sapiente nisi.</div>
+                            </div>
+                            <div className="product-more-info p-3 text-end">
+                                <Button
+                                    className='btn btn-dark btn-link text-dark'
+                                    onClick={() => setOpen(!open)}
+                                    aria-controls="example-collapse-text"
+                                    aria-expanded={open}
+                                >
+                                    More
+                                </Button>
+                                <Collapse in={open}>
+                                    <div id="example-collapse-text" className="text-start">
+                                        <p className="pt-2">
+                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+                                        terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+                                        labore wes anderson cred nesciunt sapiente ea proident.
+                                        </p>
+                                    </div>
+                                </Collapse>
+                            </div>
                         </div>
-                        <div className="product-price-cart text-white d-flex justify-content-between p-4">
-                            <div className="product-price">INR 5000</div>
-                            <div className="product-cart">Add to cart</div>
-                        </div>
+                    <div className="product-price-cart text-white d-flex justify-content-between p-4">
+                        <div className="product-price">INR 5000</div>
+                        <div className="product-cart">Add to cart <i className="bi bi-cart"></i></div>
                     </div>
-                </section>
+                </div>
             </section>
+        </section >
         </>
     )
 }
