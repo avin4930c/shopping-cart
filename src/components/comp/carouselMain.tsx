f5e3d99a4271a8e642a4875caa5cf282f5c59e8d
Avin4930c <114905330+avin4930c@users.noreply.github.com>
2024-01-28 23:41:12 +0530

    reduce landingPage load time

--- src/components/comp/carouselMain.tsx
+++ src/components/comp/carouselMain.tsx
@@ -6,18 +6,18 @@ function CarouselMain() {
                 <div className="-info">
                     <Carousel>
                         <Carousel.Item interval={3000}>
-                            <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjpt82gS8apKb0ap_q8Kd8YShr6hmkh1_x1XmuLvIhdgqc7SnKa1l_Y-SxYeN3Oz7c8QMR1p9QAp_VvB22DoB22Yz3wJnLI23kJxWHL3hR8QoqWyuSxWcDzCZLYut-JPiIEliRAFqj1zDLLnM1Kw2eDwUbgJ9RZOIGpPMAwYZk-uCMUw28mVr5J3ywI9g/s3840/horizon-call-of-the-3840x2160-582023.jpg" alt="" />
-                            <Carousel.Caption className='text-white'>
-                                <h3>Discover Exciting Gaming Adventures!</h3>
-                                <p>🎮 Immerse Yourself in the Latest Releases and Must-Have Titles 🚀</p>
-                                <button className="btn btn-primary p-2">Explore Now</button>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item interval={3000}>
                             <img src="https://media.cnn.com/api/v1/images/stellar/prod/i-stock-1287493837-1.jpg?c=16x9&q=h_833,w_1480,c_fill" alt="" />
                             <Carousel.Caption>
                                 <h3>Embark on Epic Journeys</h3>
                                 <p>Experience Thrilling Adventures in Every Game.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item interval={2000}>
+                            <img src="https://images2.alphacoders.com/985/985249.png" alt="" />
+                            <Carousel.Caption>
+                                <h3>Upgrade Your Gaming Setup</h3>
+                                <p>🎮 Elevate Your Experience with the Latest Gear and Accessories 🕹️</p>
+                                <button className="btn btn-primary p-2">Shop Now</button>
                             </Carousel.Caption>
                         </Carousel.Item>
                         <Carousel.Item interval={3000}>
@@ -27,12 +27,12 @@ function CarouselMain() {
                                 <p>Explore Stunning Landscapes and Unleash Your Imagination.</p>
                             </Carousel.Caption>
                         </Carousel.Item>
-                        <Carousel.Item interval={2000}>
-                            <img src="https://images2.alphacoders.com/985/985249.png" alt="" />
-                            <Carousel.Caption>
-                                <h3>Upgrade Your Gaming Setup</h3>
-                                <p>🎮 Elevate Your Experience with the Latest Gear and Accessories 🕹️</p>
-                                <button className="btn btn-primary p-2">Shop Now</button>
+                        <Carousel.Item interval={3000}>
+                            <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjpt82gS8apKb0ap_q8Kd8YShr6hmkh1_x1XmuLvIhdgqc7SnKa1l_Y-SxYeN3Oz7c8QMR1p9QAp_VvB22DoB22Yz3wJnLI23kJxWHL3hR8QoqWyuSxWcDzCZLYut-JPiIEliRAFqj1zDLLnM1Kw2eDwUbgJ9RZOIGpPMAwYZk-uCMUw28mVr5J3ywI9g/s3840/horizon-call-of-the-3840x2160-582023.jpg" alt="" />
+                            <Carousel.Caption className='text-white'>
+                                <h3>Discover Exciting Gaming Adventures!</h3>
+                                <p>🎮 Immerse Yourself in the Latest Releases and Must-Have Titles 🚀</p>
+                                <button className="btn btn-primary p-2">Explore Now</button>
                             </Carousel.Caption>
                         </Carousel.Item>
                     </Carousel>
